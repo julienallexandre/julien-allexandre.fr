5e4fafe721873abeb3ececdd187587df45465f9d
Julien Allexandre <contact@julien-allexandre.fr>
2022-07-18 11:44:31 +0200

    Creation of the navigation menu and presentation section (#5)
    * feat: addition of reset.css and fonts, configuration of the basic style
    
    * feat: creation of the mobile navigation
    
    * feat: creation of the desktop navigation
    
    * feat: creation of the mobile presentation section
    
    * feat: creation of the desktop presentation section
    
    * feat: adding the ide files

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Julien Allexandre | Portfolio</title>
+    <link rel="stylesheet" href="assets/styles/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+<body>
+<header>
+    <nav class="menu">
+        <h4 class="menu__logo">JA</h4>
+        <ul class="menu__bar" id="menu_bar">
+            <li class="menu__item">
+                <a href="" class="menu__link">Compétences</a>
+            </li>
+            <li class="menu__item">
+                <a href="" class="menu__link">Réalisations</a>
+            </li>
+            <li class="menu__item menu__contact">
+                <a href="" class="menu__link">Me contacter</a>
+            </li>
+        </ul>
+        <div class="menu__mobile" id="menu_mobile">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </nav>
+</header>
+<section class="presentation">
+    <div class="presentation__img">
+        <img src="assets/images/singe.png" alt="Image d'un singe">
+    </div>
+    <div class="presentation__bloc">
+        <div class="presentation__text">
+            <p class="presentation__text-title">Bonjour, je suis</p>
+            <h1 class="presentation__text-name">Julien Allexandre</h1>
+            <p class="presentation__text-job">Développeur Web PHP</p>
+            <p class="presentation__text-info">Je suis à l'écoute de toutes opportunités dans le développement web.</p>
+        </div>
+        <div class="presentation__btn">
+            <a href="" class="presentation__btn-link">Mes réalisations</a>
+            <a href="" class="presentation__btn-link">Me contacter</a>
+        </div>
+    </div>
+</section>
+
+<script src="assets/js/index.js"></script>
+</body>
+</html>
